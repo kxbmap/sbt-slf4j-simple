f355ba91c8135e6d964a631264b691d7e22b95e1
kxbmap <kxbmap@gmail.com>
2018-06-23 03:50:19 +0900

    Add sbt.Level to Slf4jSimpleLogLevel conversion

--- src/main/scala/sbtslf4jsimple/Slf4jSimpleLogLevel.scala
+++ src/main/scala/sbtslf4jsimple/Slf4jSimpleLogLevel.scala
@@ -1,5 +1,7 @@
 package sbtslf4jsimple
 
+import scala.language.implicitConversions
+
 sealed abstract class Slf4jSimpleLogLevel(override val toString: String)
 
 object Slf4jSimpleLogLevel {
@@ -10,5 +12,13 @@ object Slf4jSimpleLogLevel {
   case object Warn extends Slf4jSimpleLogLevel("warn")
   case object Error extends Slf4jSimpleLogLevel("error")
   case object Off extends Slf4jSimpleLogLevel("off")
+
+
+  implicit def sbtLevelToLogLevel(level: sbt.Level.Value): Slf4jSimpleLogLevel = level match {
+    case sbt.Level.Debug => Debug
+    case sbt.Level.Info => Info
+    case sbt.Level.Warn => Warn
+    case sbt.Level.Error => Error
+  }
 
 }
